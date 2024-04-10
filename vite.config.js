9d7d35b33bf7bce43c49e7995dafd6e9cc03d28c
Pinyi <wpypy8@gmail.com>
2024-04-11 01:10:46 +0800

    測試gh-page問題

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import UnoCSS from 'unocss/vite';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
+const INVALID_CHAR_REGEX = /[\x00-\x1F\x7F<>*#"{}|^[\]`;?:&=+$,]/g;
+const DRIVE_LETTER_REGEX = /^[a-z]:/i;
+
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/dk_pratice_backstage/',
@@ -9,4 +12,18 @@ export default defineConfig({
     vue(),
     UnoCSS(),
   ],
+  build: {
+    rollupOptions: {
+      output: {
+        sanitizeFileName(name) {
+          const match = DRIVE_LETTER_REGEX.exec(name);
+          const driveLetter = match ? match[0] : '';
+          return (
+            driveLetter +
+            name.slice(driveLetter.length).replace(INVALID_CHAR_REGEX, "")
+          );
+        },
+      },
+    },
+  }
 })
