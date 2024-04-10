a615ce894910bc8c077855e5762ca16d722c981d
Pinyi <wpypy8@gmail.com>
2024-04-10 11:14:08 +0800

    Updates

--- assets/BackStage-BmameCxT.js
+++
@@ -1,1 +0,0 @@
-import{c as t,o as c}from"./index-UWncGDeY.js";const e={class:"text-25px text-#ff6700 bg-#ccc"},n={__name:"BackStage",setup(s){return(o,a)=>(c(),t("div",e,"你好Unocss"))}};export{n as default};
